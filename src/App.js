b92a3f27cda5ae8cb91abcbb13f45f615e504e47
tacowasabii <godseoril812@gmail.com>
2022-11-26 03:15:45 +0900

    add onclick prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import HandIcon from "./HandIcon";
+import HandButton from "./HandButton";
 
 function App() {
+  const handleClick = (value) => console.log(value);
   return (
     <div>
-      <HandIcon value="rock" />
-      <HandIcon value="scissor" />
-      <HandIcon value="paper" />
+      <HandButton value="rock" onClick={handleClick} />
+      <HandButton value="scissor" onClick={handleClick} />
+      <HandButton value="paper" onClick={handleClick} />
     </div>
   );
 }
